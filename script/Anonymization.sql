4a19db871f5d282cb47d0592ed4658e62e141032
decaldaraaa <kuntohidayat20@student.uns.ac.id>
2026-04-16 17:46:09 +0700

    second

--- script/Anonymization.sql
+++ script/Anonymization.sql
@@ -1,67 +1,26 @@
--- 1. Pindah ke database Mandat
-USE Mandat;
-GO
-
--- 2. Buat LOGIN baru di level server (Ganti nama jika ingin yang lain)
-CREATE LOGIN user_mandat 
-WITH PASSWORD = 'MAVIAXX22', 
-     DEFAULT_DATABASE = Mandat;
-GO
-
--- 3. Buat USER di dalam database Mandat untuk login tersebut
-CREATE USER user_mandat FOR LOGIN user_mandat;
-GO
-
--- 4. Berikan akses sebagai owner agar bebas melakukan ETL di Pentaho
-ALTER ROLE db_owner ADD MEMBER user_mandat;
-GO
-
-ALTER TABLE Data_Siswa 
-ALTER COLUMN Nama_Lengkap VARCHAR(50);
-
 CREATE FUNCTION fn_Masking_NISN (@NISN VARCHAR(20))
 RETURNS VARCHAR(20)
 AS
 BEGIN
     DECLARE @MaskedNISN VARCHAR(20);
     
-    -- Jika panjang NISN lebih dari 3, mask sisanya dengan '*'
     IF LEN(@NISN) > 3
         SET @MaskedNISN = REPLICATE('*', LEN(@NISN) - 3) + RIGHT(@NISN, 3);
     ELSE
-        SET @MaskedNISN = @NISN; -- Jaga-jaga jika data kosong/terlalu pendek
+        SET @MaskedNISN = @NISN;
         
     RETURN @MaskedNISN;
-END;
-GO
-
-CREATE FUNCTION fn_Masking_Nama (@Nama VARCHAR(100))
-RETURNS VARCHAR(100)
-AS
-BEGIN
-    DECLARE @MaskedNama VARCHAR(100);
-    
-    -- Menampilkan huruf pertama saja, sisanya diubah jadi '*'
-    IF LEN(@Nama) > 1
-        SET @MaskedNama = LEFT(@Nama, 1) + REPLICATE('*', LEN(@Nama) - 1);
-    ELSE
-        SET @MaskedNama = @Nama;
-        
-    RETURN @MaskedNama;
 END;
 GO
 
 CREATE PROCEDURE sp_GetDataSiswa_Aman
 AS
 BEGIN
-    -- Mencegah pesan jumlah baris (opsional, untuk performa)
     SET NOCOUNT ON;
 
     SELECT 
-        -- Menggunakan fungsi masking untuk data sensitif
         dbo.fn_Masking_NISN(NISN) AS NISN_Aman,
         
-        -- Kolom yang sudah di-noise (dari tahap sebelumnya) atau aman
 		Nama_Lengkap,
         Penghasilan_Keluarga_Bulan,
         Jarak_Ke_Sekolah_KM,
@@ -74,9 +33,6 @@ BEGIN
 END;
 GO
 
-DROP FUNCTION fn_masking_nama
-
-DROP PROCEDURE sp_GetDataSiswa_Aman
-DROP FUNCTION fn_masking_nama
-
 EXEC sp_GetDataSiswa_Aman;
+
+SELECT * FROM Data_Siswa
